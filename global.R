4de53789d0a0118ec8b57b85d78276479edbe39c
matthgray <matthewlearns2@gmail.com>
2020-12-15 19:26:06 -0600

    added comments

--- global.R
+++ global.R
@@ -2,9 +2,9 @@ library(shiny)
 library(tidyverse)
 library(shinydashboard)
 
-# penguins <- read_csv('data/penguins.csv')
-# 
-# island_choices <- sort(unique(penguins$island))
+# upload should be what ever the cencus data is
+# This data will show on the top row of the application
+# Should be 2 columns and 29 rows
 
 census_data <- read_csv('upload.csv')
 
